2291ecb775f841bbc0892928dba2dc1014388dba
Robert Watts <robertwatts@Roberts-MacBook-Pro.local>
2017-01-21 13:46:27 -0500

    improve youtube functionality

--- assets/javascript/googlemaps.js
+++ assets/javascript/googlemaps.js
@@ -1,6 +1,21 @@
 var map;
 var infoWindow;
 var service;
+function getLocation(){
+  if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(function (position) {
+          console.log(position);
+    });
+  } else{
+  }
+};
+function showPosition(position) {
+    var userLatitude=position.coords.latitude;
+    var userLongitude=position.coords.longitude; 
+    console.log(userLatitude);
+    console.log(userLongitude);
+};
+getLocation();
 function initMap() {
   console.log("calling map")
   map = new google.maps.Map(document.getElementById('map'), {
@@ -13,16 +28,12 @@ function initMap() {
     stylers: [{ visibility: 'off' }]
     }]
   });
-
   infoWindow = new google.maps.InfoWindow();
   service = new google.maps.places.PlacesService(map);
-
   //The idle event is a debounced event, so we can query & listen without
   //throwing too many requests at the server.
   //map.addListener('idle', performSearch);
   }
-
-
 function callAjax (){
   var keywordName=$("#searchinput").val();
   var queryURL="https://maps.googleapis.com/maps/api/geocode/json?address=-33.8670522,151.1957362&radius=500&type=gym&keyword=" + keywordName + "&key=AIzaSyBtd0Rm6EoowNo16BoCBpQwxqgv40Z5P0U"
@@ -41,7 +52,6 @@ function callAjax (){
                   service.radarSearch(request, callback);
                 }
                 performSearch();
-
                 function callback(results, status) {
                   if (status !== google.maps.places.PlacesServiceStatus.OK) {
                     console.error(status);
@@ -51,7 +61,6 @@ function callAjax (){
                     addMarker(result);
                   }
                 }
-
                 function addMarker(place) {
                   var marker = new google.maps.Marker({
                     map: map,
@@ -62,7 +71,6 @@ function callAjax (){
                       scaledSize: new google.maps.Size(10, 17)
                     }
                   });
-
                   google.maps.event.addListener(marker, 'click', function() {
                     service.getDetails(place, function(result, status) {
                       if (status !== google.maps.places.PlacesServiceStatus.OK) {
@@ -76,12 +84,9 @@ function callAjax (){
                 }            
       });
 };
-
 $("#searchBtn").click( function(){
   callAjax();
 });
-
-
 $("#searchBtn").on("click", function(){
   console.log("hello");
   event.preventDefault();
